a0117c1a8d06c40806dd3b8e8dabb62850f3e40f
T-Nasser <tar.zid_nass@hotmail.com>
2026-07-16 11:27:21 -0300

    Ajuste do header e footer pra w-7xl

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <>
 
-      <div className="bg-gray-800 flex gap-29 justify-center items-center py-2">
+      <div className="w-7xl w-full mx-auto bg-gray-800 py-6 justify-around flex flex-row">
         <img src="https://images.vexels.com/media/users/3/142789/isolated/preview/2bfb04ad814c4995f0c537c68db5cd0b-logotipo-do-circulo-multicolorido.png" alt="" className="w-26" />
         <div className="text-white text-2xl text-center ">Menu 01
           <ul className="text-gray-500 text-center">
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <>
-            <div className="w-7xl w-full grid grid-cols-2 bg-gray-800 py-6 justify-center">
-                <div className="w-full h-20 flex items-center justify-center">
+            <div className="w-7xl w-full mx-auto bg-gray-800 py-6">
+                <div className="flex flex-row justify-around gap-26 w-full">
                     <img src="https://images.vexels.com/media/users/3/142789/isolated/preview/2bfb04ad814c4995f0c537c68db5cd0b-logotipo-do-circulo-multicolorido.png" alt="" className="w-20" />
-                </div>
-                <div className="grid grid-cols-2 gap-26 w-full right-0">
                     <div className="w-30 h-20 rounded-lg border border-black bg-cyan-900 flex justify-center items-center content-center">
                         <Link to="/"><h1 className="text-2xl">Home</h1></Link>
                     </div>
