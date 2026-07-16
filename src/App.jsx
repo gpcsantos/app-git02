51c45fabe4b4577d8c1852343f66d675f359e32b
buzinei-bibi <biancavtsilva@gmail.com>
2026-07-16 10:23:19 -0300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Produtos from "./pages/Produtos/Produtos";
+
 function App() {
   return (
     <>
-      <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
-        <h1 className="text-2xl font-bold">Projeto BASE</h1>
-      </div>
+      <Produtos />
     </>
   );
 }
