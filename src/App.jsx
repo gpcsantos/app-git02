fd1f064434ff9ec678fe42317e5ece2f236c4b10
HECTOR LUCIANO LORENO MORO <hector.llmoro@senacsp.edu.br>
2026-07-16 09:17:42 -0300

    Home Escrita

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <>
       <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
         <h1 className="text-2xl font-bold">Projeto BASE</h1>
+        <div>Home</div>
       </div>
     </>
   );
