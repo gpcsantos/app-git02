7e92de6ef7c2c7548c8f6c53c805d491be5f18ee
T-Nasser <tar.zid_nass@hotmail.com>
2026-07-16 10:29:04 -0300

    Primeiro Commit - Criação do Header e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
 function App() {
   return (
     <>
+    <Header />
       <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
         <h1 className="text-2xl font-bold">Projeto BASE</h1>
       </div>
+    <Footer />
     </>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+        <>
+            <div className="w-full grid grid-cols-2 bg-gray-800 py-6 justify-center">
+                <div className="w-full h-20 flex items-center justify-center">
+                    <img src="https://images.vexels.com/media/users/3/142789/isolated/preview/2bfb04ad814c4995f0c537c68db5cd0b-logotipo-do-circulo-multicolorido.png" alt="" className="w-20" />
+                </div>
+                <div className="grid grid-cols-2 gap-26 w-full right-0">
+                    <div className="w-30 h-20 rounded-lg border border-black bg-cyan-900 flex justify-center items-center content-center">
+                        <a href=""><h1 className="text-2xl">Home</h1></a>
+                    </div>
+                    <div className="w-30 h-20 rounded-lg border border-black bg-cyan-900 flex justify-center items-center content-center">
+                        <a href=""><h1 className="text-2xl">Produtos</h1></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
